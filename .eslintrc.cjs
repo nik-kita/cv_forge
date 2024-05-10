839b32162d64a8b9e100111861201ce4727b329e
nik-kita <nik-kita.deno.dev>
2024-05-10 15:17:48 +0200

    styles: primevue, tailwindcss

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,23 +3,18 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  overrides: [
-    {
-      files: [
-        'e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:playwright/recommended'
-      ]
-    }
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    semi: ['error', 'never'],
+    'vue/multi-word-component-names': 'off',
+    'vue/no-reserved-component-names': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import './assets/main.css'
+// ---
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import Button from 'primevue/button'
+import PrimeVue from 'primevue/config'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(router)
-
+app.use(PrimeVue, {ripple: true})
+app.component('Button', Button)
 app.mount('#app')

7181b5f21464b77f4303c1ef5fe70eb191656bc4
nik-kita <nik.deno.dev@gmail.com>
2024-05-24 10:46:29 +0200

    fix: rm nik

--- src/api/api_get_all_profiles.ts
+++ src/api/api_get_all_profiles.ts
@@ -2,8 +2,6 @@ import {req} from './req'
 
 export const api_get_all_profiles = ({
   access_token,
-  nik,
 }: {
   access_token: string
-  nik: string
 }) => req.get<'/profiles/'>('/profiles', access_token)

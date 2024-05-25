728d044bb05c4cd0e3c2edd4641f8b09cf9813ad
nik-kita <nik.deno.dev@gmail.com>
2024-05-25 18:30:03 +0200

    page_profiles_machine

--- src/pages/PageProfiles.vue
+++ src/pages/PageProfiles.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+import {use_x} from '@/use_x'
+import {use_xstore} from '@/x/xstore'
+import {onUnmounted, ref} from 'vue'
+import type {ActorRefFrom} from 'xstate'
+const {nav} = use_x()
+const {is_user} = use_xstore()
+const page_actor = nav.getSnapshot().children
+  .page_profiles! as ActorRefFrom<x.page_profiles.logic>
+const state = ref(page_actor.getSnapshot().value)
+
+onUnmounted(
+  page_actor.subscribe(s => {
+    const sv = s.value
+    state.value = sv
+  }).unsubscribe,
+)
+</script>
+
 <template>
   <h1>Profiles</h1>
+  <pre>{{ state }}</pre>
 </template>
--- src/x/page_profiles/page_profiles_machine.ts
+++ src/x/page_profiles/page_profiles_machine.ts
@@ -1,15 +1,23 @@
 import {api_get_all_profiles} from '@/api/api_get_all_profiles'
+import {api_get_all_public_profiles_by_nik} from '@/api/api_get_all_public_profiles_by_nik'
 import {get_access_token} from '@/local_storage/persistent.tokens'
-import {assertEvent, assign, setup} from 'xstate'
+import {watch} from 'vue'
+import {assign, setup} from 'xstate'
 import {api_to_fetch_logic} from '../utils/api_to_fetch_logic'
+import {use_xstore} from '../xstore'
 
 export const page_profiles_machine = setup({
   types: {
     context: {} as {
-      explored_public_profiles?: {}
+      explored_public_profiles?: ApiRes<
+        'get',
+        '/profiles/public/{nik}'
+      >
       my_profiles?: ApiRes<'get', '/profiles/'>
     },
     events: {} as
+      | {type: 'page_profiles.reset_machine'}
+      | {type: 'page_profiles.public_nik.not_found'}
       | {type: 'page_profiles.toggle_select'}
       | {type: 'page_profiles.select.rm'}
       | {type: 'page_profiles.select.cancel'}
@@ -17,11 +25,14 @@ export const page_profiles_machine = setup({
       | {type: 'page_profiles.config_rm_selected.no'}
       | {
           type: 'global.update_nik_route_param'
-          payload: ApiRes<'get', '/profiles/'>
         }
       | {
           type: 'page_profiles.get_all_profiles.success'
           payload: ApiRes<'get', '/profiles/'>
+        }
+      | {
+          type: 'page_profiles.get_all_public_profiles.success'
+          payload: ApiRes<'get', '/profiles/public/{nik}'>
         },
   },
   actions: {
@@ -29,9 +40,40 @@ export const page_profiles_machine = setup({
     api_get_all_public_profiles_by_nik: function ({
       context,
       event,
+      self,
+      system,
     }) {
-      // Add your action code here
-      // ...
+      const {nik_curr_route_param} = use_xstore()
+      if (!nik_curr_route_param.value) {
+        self.send({
+          type: 'page_profiles.public_nik.not_found',
+        })
+
+        return
+      }
+      api_to_fetch_logic(
+        () => {
+          return api_get_all_public_profiles_by_nik({
+            nik: nik_curr_route_param.value!,
+          })
+        },
+        {
+          self,
+          system,
+          emit_on_success: res => {
+            return {
+              type: 'page_profiles.get_all_public_profiles.success',
+              payload: res,
+            }
+          },
+          emit_on_fail: err => {
+            return {
+              type: 'page_profiles.public_nik.not_found',
+            }
+          },
+          is_access_token_required: false,
+        },
+      )
     },
     api_rm_profiles: function ({context, event}) {
       // Add your action code here
@@ -57,9 +99,6 @@ export const page_profiles_machine = setup({
               payload: res,
             }
           },
-          emit_on_fail: err => {
-            return {} as any
-          },
           is_access_token_required: true,
           system,
           self,
@@ -69,8 +108,9 @@ export const page_profiles_machine = setup({
   },
   guards: {
     is_owner: function ({context, event}) {
-      // Add your guard condition here
-      return true
+      const {viewer_role, is_user} = use_xstore()
+
+      return is_user && viewer_role.value !== 'viewer'
     },
     is_some_select: function ({context, event}) {
       // Add your guard condition here
@@ -78,10 +118,22 @@ export const page_profiles_machine = setup({
     },
   },
 }).createMachine({
-  context: {},
+  /** @xstate-layout N4IgpgJg5mDOIC5QAcCGMD6yBOB7AZgJYA2cGAtqgMYAWhAdmAMRTG4BGqxAdAK7IRUAFzAZ6hANYY8vEVlTZU5ANoAGALqIUuWISGFc9LSAAeiALQBWVQDZuATkf2AHJYDMARgAstm5YA0IACeiH52HgDs9jbO9h62Hs5uNgC+KYFomDgEJGSUtAzMmaLZRKSw3DBCGFzEWHhlcNywvFRUcLBqmkggyDp6BkY9ZgiR8dxebhFuTvGq9l6BIQjmAEyT3BE2qjOWsfarq6qWaRnoJQ25sBTUdIzcAKL0IthMXcZ9uvqGxiMelpZVtwPAdLPYZhF5nElogwZYHO54qtoqt-jY3Kdeud6jlyjcCvcni83h5utovoNfohUR47JYvF5-m5LB5UQDFsFYfZ4dy3KofJNYhEIid0lispc8fk7mBuAARQiwNDEIIYXAAd3oOMasCYxW1V24QlwUFYolgYFIVCE7x6nwGP2G1NszmBoOcxzcXgObmcMJWvomTgiznWXhFrlFZwluLyt0K3AAypawNa9djSobjabSBgLVabRoPv1vkNQH8GV5uIDVB7Vr6pmD-asIlWbO2bIzou24vTMfrM1L4-dkwX0zGdUaTWa8ynrcpScWKY7y4hIpDuH5nFF+VsIqtYv74vCvHs+aoL24jnEMWKB5K4wTZaPU0JxxdYxV86-uNgVEW7RLSknQQTwqzBKYLxBVYwQvGx-TWTZa19ZxvBFWthVrfsMwfa5pQTF803vT9mjnIRuCoVB6HaYhbXJB0y1MRBwSBaCbHiDx-iRNx-WZeEkjcQTVFbesvAPCJsInK58RlbgAGFDCIP9pHIWcC0gA1ynfTSmioRTCCgFS1NfSBuCCOA6N6ICVyYhAfHhdDhJsewRWZdF4M5Oza2rUTWVWGJ-ijcUPx1GSEwU+glNU5Tv2tDTBzgbSEoqPTIoMozYpECBuHoXBLPtUsqTs7YJlpETUVcdFW14kFq0Zdj4hcRkjlvMVcogOAPhwz8wsYJcGKKqx6QcJxXDA3wAk88xWR5YN91pQ51lvaMQuk-DCWeMBsH6wqQNDCJuBbQS+I9RqPH9dxVG4VRWWiK9vDBfdJNWocn3lRVlVVDUtWSnbgNXUYxs3FzQ2scEI3sf1Jn4wTkWmFzO3rILiNC9bZQVJViFQVVkF4dhiEIKgdPgQDl0YkZUR8bhkgZZwxO5BYOWWKIeWRGx-IOexhNZZ7id658yL+myRicBxpmcdE2dPGCj3BCY9jhiJOJDGJUjvbrUeHWUIqijKyPi3ChfJiwQVdMFnAlmYGfrFwavsOraU4+Y6Z5tIUiAA */
+  context: ({self}) => {
+    const {nik_curr_route_param} = use_xstore()
+
+    watch(nik_curr_route_param, () => {
+      self.send({type: 'page_profiles.reset_machine'})
+    })
+
+    return {}
+  },
   id: 'page_profiles_machine',
   initial: 'Enter',
   on: {
+    'page_profiles.reset_machine': {
+      target: '.Enter',
+    },
     'global.update_nik_route_param': {
       target: '#page_profiles_machine.Enter',
     },
@@ -93,6 +145,7 @@ export const page_profiles_machine = setup({
     Enter: {
       always: [
         {
+          reenter: true,
           target: 'Dispaly_own_profiles',
           actions: {
             type: 'api_get_all_profiles',
@@ -102,6 +155,7 @@ export const page_profiles_machine = setup({
           },
         },
         {
+          reenter: true,
           target: 'Display_public_profiles',
           actions: {
             type: 'api_get_all_public_profiles_by_nik',
@@ -119,7 +173,18 @@ export const page_profiles_machine = setup({
         },
       },
     },
-    Display_public_profiles: {},
+    Display_public_profiles: {
+      on: {
+        'page_profiles.public_nik.not_found': {
+          target: '.Propose_to_explore',
+        },
+      },
+      initial: 'Display',
+      states: {
+        Propose_to_explore: {},
+        Display: {},
+      },
+    },
     Select: {
       on: {
         'page_profiles.toggle_select': [

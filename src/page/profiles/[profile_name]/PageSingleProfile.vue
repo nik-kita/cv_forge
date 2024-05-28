933ac65f86e693e6be7c8664f8b6d8adbe9b6889
nik-kita <nik.deno.dev@gmail.com>
2024-05-28 17:32:00 +0200

    new trouble demonstration

--- src/page/profiles/[profile_name]/PageSingleProfile.vue
+++ src/page/profiles/[profile_name]/PageSingleProfile.vue
@@ -1,23 +1,46 @@
 <script setup lang="ts">
 import {use_x} from '@/use_x'
 import {onUnmounted, ref, watch} from 'vue'
-import type {Subscription} from 'xstate'
+import type {StateValueFrom} from 'xstate'
 
 const {page_profiles} = use_x()
-let unsubscribe: Subscription | undefined = undefined
-const state = ref()
-watch(page_profiles, actor => {
-  if (unsubscribe || !actor) return
+let unsubscribe: (() => void) | undefined = undefined
+const profiles_state =
+  ref<StateValueFrom<x.page_profiles.logic>>()
+const single_profile_state =
+  ref<StateValueFrom<x.page_single_profile.logic>>()
+watch(
+  page_profiles,
+  actor => {
+    if (unsubscribe || !actor) return
 
-  unsubscribe = actor.subscribe(s => {
-    state.value = s.value
-  })
-})
+    let unsubscribe_single: (() => void) | undefined =
+      undefined
+    const unsubscribe_profiles = actor.subscribe(s => {
+      profiles_state.value = s.value
+      const single_actor = s.children.single_profile!
+      single_profile_state.value =
+        single_actor.getSnapshot().value
+      unsubscribe_single = single_actor.subscribe(ss => {
+        single_profile_state.value = ss.value
+      }).unsubscribe
+    }).unsubscribe
+
+    unsubscribe = () => {
+      unsubscribe_profiles
+      if (unsubscribe_single) unsubscribe_single()
+    }
+  },
+  {immediate: true},
+)
 
 onUnmounted(unsubscribe ? unsubscribe : () => {})
 </script>
 
 <template>
   <h1>Single profile</h1>
-  <pre>{{ state }}</pre>
+  <pre>profiles: {{ profiles_state }}</pre>
+  <pre>
+single_profile_state: {{ single_profile_state }}</pre
+  >
 </template>
--- src/page/profiles/[profile_name]/PageSingleProfile.x.ts
+++ src/page/profiles/[profile_name]/PageSingleProfile.x.ts
@@ -1,10 +1,35 @@
+import {use_xstore} from '@/common_xstate/xstore'
 import {setup} from 'xstate'
 
-export const PageSingleProfile_machine = setup(
-  {},
-).createMachine({
-  initial: 'Idle',
+export const PageSingleProfile_machine = setup({
+  guards: {
+    is_own_profile() {
+      const {is_user, nik, nik_curr_route_param} =
+        use_xstore()
+
+      return (
+        is_user.value &&
+        (!nik_curr_route_param.value ||
+          nik_curr_route_param.value === nik.value)
+      )
+    },
+  },
+}).createMachine({
+  id: 'page_single_profile_machine',
+  initial: 'Init',
   states: {
-    Idle: {},
+    Init: {
+      always: [
+        {
+          target: 'Own',
+          guard: 'is_own_profile',
+        },
+        {
+          target: 'Public',
+        },
+      ],
+    },
+    Public: {},
+    Own: {},
   },
 })
